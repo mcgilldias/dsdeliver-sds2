f7bf0ab78e51cfe9d58a3e0ea827a2ea2cf8acfc
Mcgill Dias <mcgill.dias@gmail.com>
2021-01-04 16:47:48 -0300

    Domain model

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -0,0 +1,55 @@
+package com.devsuperior.dsdeliver.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_order")
+@Data
+@NoArgsConstructor
+public class Order implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String address;
+	private Double latitude;
+	private Double longitude;
+	private Instant moment;
+	private OrderStatus status;
+
+	@ManyToMany
+	@JoinTable(
+			name = "tb_order_product", 
+			joinColumns = @JoinColumn(name = "order_id"), 
+			inverseJoinColumns = @JoinColumn(name = "product_id")
+	)
+	private Set<Product> products = new HashSet<>();
+
+	public Order(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status) {
+		super();
+		this.id = id;
+		this.address = address;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.moment = moment;
+		this.status = status;
+	}
+
+}
---
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Product.java
@@ -0,0 +1,32 @@
+package com.devsuperior.dsdeliver.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_product")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String name;
+	private Double price;
+	private String description;
+	private String imageUri;
+		
+}
